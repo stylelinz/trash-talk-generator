89857144b1585812ea2d3905872851dfcccd4cbe
stylelinz <cwchuang1997@gmail.com>
2021-04-30 23:30:27 +0800

    feat: add generator function #20210430002

---
+++ trash-talk-generator.js
@@ -0,0 +1,25 @@
+module.exports = function getTrashTalk (careerOption) {
+  const career = {
+    engineer: {
+      name: '工程師',
+      task: ['加個按鈕', '加新功能', '切個版', '改一點 code']
+    },
+    designer: {
+      name: '設計師',
+      task: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計']
+    },
+    entrepreneur: {
+      name: '創業家',
+      task: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+    }
+  }
+
+  const phrase = ['很簡單', '很容易', '很快', '很正常']
+
+  function getRandomItemInArray (arr) {
+    // '~~' == Math.floor()
+    return arr[~~(Math.random() * arr.length)]
+  }
+
+  return `身為一個${career[careerOption].name}，${getRandomItemInArray(career[careerOption].task)}，${getRandomItemInArray(phrase)}吧!`
+}
