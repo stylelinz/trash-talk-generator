c2a8c7687e62f4fff85e9d915d1a038f3be981aa
stylelinz <cwchuang1997@gmail.com>
2021-05-01 10:50:20 +0800

    feat: add post router + app.js #20210501001

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const getTrashTalk = require('./trash-talk-generator')
 
 const app = express()
 const port = 3000
@@ -13,8 +14,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  // const  = req.body
-  const trashTalk = trashTalkGenerator()
+  const { career } = req.body
+  const trashTalk = getTrashTalk(career)
+  return res.render('index', { trashTalk })
 })
 
 app.listen(port, () => console.log(`Express is listening on http://localhost:${port}`))
